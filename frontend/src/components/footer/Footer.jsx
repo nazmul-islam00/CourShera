b5d69768c42eef2e7f45a8a1f1573457b036f157
Nazmul <155834778+nazmul-islam00@users.noreply.github.com>
2026-03-26 00:08:19 +0600

    added hyperlinks

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -21,43 +21,43 @@ export const Footer = () => {
             <h2>Coursera</h2>
             <ul>
               <li>
-                <a href="#">About</a>
+                <a href="https://www.coursera.org/about">About</a>
               </li>
               <li>
-                <a href="#">What We Offer</a>
+                <a href="https://www.coursera.org/about/how-coursera-works/">What We Offer</a>
               </li>
               <li>
-                <a href="#">Leadership</a>
+                <a href="https://blog.coursera.org/leadership/">Leadership</a>
               </li>
               <li>
-                <a href="#">Careers</a>
+                <a href="https://careers.coursera.com/homepage">Careers</a>
               </li>
               <li>
                 <a href="#">Catalog</a>
               </li>
               <li>
-                <a href="#">Coursera Plus</a>
+                <a href="https://www.coursera.org/courseraplus">Coursera Plus</a>
               </li>
               <li>
-                <a href="#">Professional Certificates</a>
+                <a href="https://www.coursera.org/professional-certificates">Professional Certificates</a>
               </li>
               <li>
-                <a href="#">MasterTrack® Certificates</a>
+                <a href="https://www.coursera.org/mastertrack">MasterTrack® Certificates</a>
               </li>
               <li>
-                <a href="#">Degrees</a>
+                <a href="https://www.coursera.org/degrees">Degrees</a>
               </li>
               <li>
-                <a href="#">For Enterprise</a>
+                <a href="https://www.coursera.org/business?utm_campaign=website&utm_content=corp-to-home-footer-for-enterprise&utm_medium=coursera&utm_source=enterprise">For Enterprise</a>
               </li>
               <li>
-                <a href="#">For Government</a>
+                <a href="https://www.coursera.org/government?utm_campaign=website&utm_content=corp-to-home-footer-for-government&utm_medium=coursera&utm_source=enterprise">For Government</a>
               </li>
               <li>
-                <a href="#">For Campus</a>
+                <a href="https://www.coursera.org/campus?utm_campaign=website&utm_content=corp-to-home-footer-for-campus&utm_medium=coursera&utm_source=enterprise">For Campus</a>
               </li>
               <li>
-                <a href="#">Become a Partner</a>
+                <a href="https://www.coursera.org/partnerships?utm_medium=coursera&utm_source=partnerships&utm_campaign=website&utm_content=corp-to-home-footer-become-a-partner">Become a Partner</a>
               </li>
             </ul>
           </div>
@@ -66,25 +66,19 @@ export const Footer = () => {
             <h2>Community</h2>
             <ul>
               <li>
-                <a href="#">Learners</a>
+                <a href="https://www.coursera.support/s/community?language=en_US">Learners</a>
               </li>
               <li>
-                <a href="#">Partners</a>
+                <a href="https://www.coursera.org/partners">Partners</a>
               </li>
               <li>
-                <a href="#">Beta Testers</a>
+                <a href="https://www.coursera.support/s/article/learner-000001137?language=en_US">Beta Testers</a>
               </li>
               <li>
-                <a href="#">Translators</a>
+                <a href="https://blog.coursera.org">Blog</a>
               </li>
               <li>
-                <a href="#">Blog</a>
-              </li>
-              <li>
-                <a href="#">Tech Blog</a>
-              </li>
-              <li>
-                <a href="#">Teaching Center</a>
+                <a href="https://medium.com/coursera-engineering">Tech Blog</a>
               </li>
             </ul>
           </div>
@@ -93,34 +87,34 @@ export const Footer = () => {
             <h2>More</h2>
             <ul>
               <li>
-                <a href="#">Press</a>
+                <a href="https://www.coursera.org/about/press">Press</a>
               </li>
               <li>
-                <a href="#">Investors</a>
+                <a href="https://investor.coursera.com/overview/default.aspx">Investors</a>
               </li>
               <li>
-                <a href="#">Terms</a>
+                <a href="https://www.coursera.org/about/terms">Terms</a>
               </li>
               <li>
-                <a href="#">Privacy</a>
+                <a href="https://www.coursera.org/about/privacy">Privacy</a>
               </li>
               <li>
-                <a href="#">Help</a>
+                <a href="https://www.coursera.support/s/learner-help-center?language=en_US">Help</a>
               </li>
               <li>
-                <a href="#">Accessibility</a>
+                <a href="https://www.coursera.support/s/article/learner-000001052?language=en_US">Accessibility</a>
               </li>
               <li>
-                <a href="#">Contact</a>
+                <a href="https://www.coursera.org/about/contact">Contact</a>
               </li>
               <li>
-                <a href="#">Articles</a>
+                <a href="https://www.coursera.org/articles">Articles</a>
               </li>
               <li>
-                <a href="#">Directory</a>
+                <a href="https://www.coursera.org/directory">Directory</a>
               </li>
               <li>
-                <a href="#">Affiliates</a>
+                <a href="https://www.coursera.org/about/affiliates">Affiliates</a>
               </li>
             </ul>
           </div>
@@ -128,14 +122,14 @@ export const Footer = () => {
           <div className="footer-column mobile-app-col">
             <h2>Learn Anywhere</h2>
             <div className="app-badges">
-              <a href="#" className="store-badge">
+              <a href="https://apps.apple.com/us/app/coursera-grow-your-career/id736535961" className="store-badge">
                 <FontAwesomeIcon icon={faApple} className="store-icon" />
                 <div className="store-text">
                   <span className="store-sub">Download on the</span>
                   <span className="store-title">App Store</span>
                 </div>
               </a>
-              <a href="#" className="store-badge">
+              <a href="https://play.google.com/store/apps/details?id=org.coursera.android" className="store-badge">
                 <FontAwesomeIcon icon={faGooglePlay} className="store-icon" />
                 <div className="store-text">
                   <span className="store-sub">GET IT ON</span>
@@ -153,22 +147,22 @@ export const Footer = () => {
         <div className="footer-bottom">
           <p className="copyright">© 2026 Coursera Inc. All rights reserved.</p>
           <div className="social-links">
-            <a href="#" aria-label="Facebook">
+            <a href="https://www.facebook.com/Coursera" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
               <FontAwesomeIcon icon={faFacebookF} />
             </a>
-            <a href="#" aria-label="LinkedIn">
+            <a href="https://www.linkedin.com/company/coursera" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
               <FontAwesomeIcon icon={faLinkedinIn} />
             </a>
-            <a href="#" aria-label="Twitter">
+            <a href="https://x.com/coursera" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
               <FontAwesomeIcon icon={faXTwitter} />
             </a>
-            <a href="#" aria-label="YouTube">
+            <a href="https://www.youtube.com/user/coursera" target="_blank" rel="noopener noreferrer" aria-label="YouTube">
               <FontAwesomeIcon icon={faYoutube} />
             </a>
-            <a href="#" aria-label="Instagram">
+            <a href="https://www.instagram.com/coursera/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
               <FontAwesomeIcon icon={faInstagram} />
             </a>
-            <a href="#" aria-label="Tiktok">
+            <a href="https://www.tiktok.com/@coursera" target="_blank" rel="noopener noreferrer" aria-label="Tiktok">
               <FontAwesomeIcon icon={faTiktok} />
             </a>
           </div>
